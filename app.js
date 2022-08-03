263d0eebb771bfddead60828b2e50bf792066a18
lunowe <lucanoel.wegner@versicherungsforen.net>
2022-08-03 15:48:36 +0200

    basics

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-// Hallo Welt
-console.log("Hello World");
+const WIDTH = 600;
+const HEIGHT = 600;
+
+function setup() {
+  createCanvas(WIDTH, HEIGHT)
+}
+
+function draw() {
+  background(220);
+  fill(0);
+  rect(WIDTH / 2, HEIGHT / 2, 50, 50);
+}
