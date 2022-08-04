83f3d7b8b8c6212d634293b826b92e4f2a6e2510
lunowe <lucanoel.wegner@versicherungsforen.net>
2022-08-04 10:28:00 +0200

    adsf

--- Player.js
+++ Player.js
@@ -1,7 +1,27 @@
 class Player {
-  constructor() {
+  constructor(num) {
     this.name;
     this.cash = 100;
-    this.troops = [];
+    this.troops = [0,0,0,0,0];
+    this.pos = 0; // 0 - 4
+    this.height = OFFSET_Y + RECT_WIDTH * this.pos;
+    this.num = num
+  }
+
+  choose() {
+    if (this.height < OFFSET_Y + RECT_WIDTH * this.pos) {
+      this.height += 20;
+    }
+    if (this.height > OFFSET_Y + RECT_WIDTH * this.pos) {
+      this.height -= 20;
+    }
+    noFill();
+    strokeWeight(5);
+    if (this.num == 1) {
+      rect(OFFSET_X, this.height, RECT_WIDTH,RECT_WIDTH, CONTAINER_RADIUS);
+    }
+    if (this.num == 2) {
+      rect(WIDTH - OFFSET_X - RECT_WIDTH, this.height, RECT_WIDTH,RECT_WIDTH, CONTAINER_RADIUS);
+    }
   }
 }
---
+++ availableTroops.js
@@ -0,0 +1,7 @@
+class troopsPool {
+  constructor() {
+    this.troopscountListStock = [10,20,0,0,0];
+    this.troopscountList = [10,20,0,0,0];
+  }
+
+}
